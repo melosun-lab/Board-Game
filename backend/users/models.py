057669e7205aed164d066d26c19e9ec115b350e0
wangbowen <wangbowen218@gmail.com>
2020-12-20 19:11:22 +0800

    lec14

---
+++ backend/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=16)
+    nickname = models.CharField(max_length=30)
+
+# description = models.TextField(blank=True)
+# created_at = models.DateTimeField(auto_now_add=True)
+# url = models.URLField()
