8aa9f02187005199721dca7816d12d8aab7f5cba
aannmmoo <wangbowen218@gmail.com>
2020-12-24 22:26:16 +0800

    connect trial

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'rooms',
-    'users'
+    'users',
 ]
 
 GRAPHENE = {
@@ -50,14 +51,17 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 AUTHENTICATION_BACKENDS = [
     'graphql_jwt.backends.JSONWebTokenBackend',
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import ListView from './views/ListView'
+import LoginView from './views/LoginView'
+import LogoutView from './views/LogoutView'
+import CreateView from './views/CreateView'
+import DetailView from './views/DetailView'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost';
+const apiclient = new ApolloClient({
+      uri: 'http://127.0.0.1:8000/graphql/'
+    });
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={apiclient}>
+        <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/messages/create/">Create Message</Link></li>
+            <li><Link to="/login/">Login</Link></li>
+            <li><Link to="/logout/">Logout</Link></li>
+          </ul>
+          <Route exact path="/" component={ListView} />
+          <Route exact path="/login/" component={LoginView} />
+          <Route exact path="/logout/" component={LogoutView} />
+          <Switch>
+            <Route path="/messages/create/" component={CreateView} />
+            <Route path="/messages/:id/" component={DetailView} />
+          </Switch>
+        </div>
+      </Router>
+      </ApolloProvider>
+
+    )
+  }
 }
 
 export default App;
