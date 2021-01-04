172df1e9543b25087ee611127ed8ba0172a2df50
pu2Gainz <71114424+pu2Gainz@users.noreply.github.com>
2021-01-03 18:11:18 -0800

    update email
    add email to both frontedn and backend

--- backend/users/schema.py
+++ backend/users/schema.py
@@ -31,9 +31,10 @@ class CreateUser(graphene.Mutation):
         username = graphene.String(required=True)
         password = graphene.String(required=True)
         nickname = graphene.String(required=True)
-    
-    def mutate(self, info, username, password, nickname):
-        user = get_user_model()(username=username, password=password, nickname=nickname)
+        email = graphene.String(required=True)
+   
+    def mutate(self, info, username, password, nickname, email):
+        user = get_user_model()(username=username, password=password, nickname=nickname, email=email)
         user.set_password(password)
         user.save()
         return CreateUser(user=user)
@@ -47,8 +48,11 @@ class UpdateUser(graphene.Mutation):
         password = graphene.String()
         nickname = graphene.String()
         friends = graphene.String()
+        email = graphene.String()
+
+        
     
-    def mutate(self, info, id, username=None, password=None, nickname=None, friends=None):
+    def mutate(self, info, id, username=None, password=None, nickname=None, friends=None, email=None):
         user = get_user_model().objects.get(id=id)
 
         if user != info.context.user:
@@ -62,6 +66,8 @@ class UpdateUser(graphene.Mutation):
             user.nickname = nickname
         if friends:
             user.friends = friends
+        if email:
+            user.email = email
 
         user.save()
 
